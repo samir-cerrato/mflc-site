64e10a529d66b26c2934fe006b2d91ed7a97c51a
Samir Cerrato <cerrato.samir@yahoo.com>
2025-09-12 01:16:14 -0400

    seo: multi-type Organization+Church JSON-LD; move inLanguage to WebSite

--- src/app/SchemaOrg.tsx
+++ src/app/SchemaOrg.tsx
@@ -1,50 +1,66 @@
-// src/app/SchemaOrg.tsx
 "use client";
 import Script from "next/script";
 
 export default function SchemaOrg() {
   const jsonLd = {
     "@context": "https://schema.org",
-    "@type": "Church",
-    name: "Ministerio Familiar La Cosecha",
-    alternateName: "MFLC Yonkers",
-    url: "https://www.mflcyonkers.com/",
-    image: "https://www.mflcyonkers.com/og-cover.jpg",
-    logo: "https://www.mflcyonkers.com/logo.PNG",
-    address: {
-      "@type": "PostalAddress",
-      streetAddress: "191 North Broadway",
-      addressLocality: "Yonkers",
-      addressRegion: "NY",
-      postalCode: "10701",
-      addressCountry: "US",
-    },
-    sameAs: [
-      "https://www.facebook.com/ministeriofamiliarlacosecha",
-      "https://www.instagram.com/lacosechayonkers",
-      "https://www.youtube.com/@MFLCYONKERS",
+    "@graph": [
+      {
+        // Make the church also an Organization so Google can use the Logo rich result
+        "@type": ["Organization", "Church"],
+        "@id": "https://www.mflcyonkers.com/#org",
+        name: "Ministerio Familiar La Cosecha",
+        alternateName: "MFLC Yonkers",
+        url: "https://www.mflcyonkers.com/",
+        logo: {
+          "@type": "ImageObject",
+          url: "https://www.mflcyonkers.com/logo.PNG",
+        },
+        image: "https://www.mflcyonkers.com/logo.PNG",
+        sameAs: [
+          "https://www.facebook.com/ministeriofamiliarlacosecha",
+          "https://www.instagram.com/lacosechayonkers",
+          "https://www.youtube.com/@MFLCYONKERS",
+        ],
+        address: {
+          "@type": "PostalAddress",
+          streetAddress: "191 North Broadway",
+          addressLocality: "Yonkers",
+          addressRegion: "NY",
+          postalCode: "10701",
+          addressCountry: "US",
+        },
+        openingHoursSpecification: [
+          {
+            "@type": "OpeningHoursSpecification",
+            dayOfWeek: "Friday",
+            opens: "19:30",
+            closes: "21:00",
+          },
+          {
+            "@type": "OpeningHoursSpecification",
+            dayOfWeek: "Saturday",
+            opens: "19:00",
+            closes: "21:00",
+          },
+          {
+            "@type": "OpeningHoursSpecification",
+            dayOfWeek: "Sunday",
+            opens: "14:30",
+            closes: "16:30",
+          },
+        ],
+      },
+      {
+        // Put inLanguage on the WebSite (it's a CreativeWork)
+        "@type": "WebSite",
+        "@id": "https://www.mflcyonkers.com/#website",
+        url: "https://www.mflcyonkers.com/",
+        name: "MFLC Yonkers",
+        inLanguage: ["es", "en"],
+        publisher: { "@id": "https://www.mflcyonkers.com/#org" },
+      },
     ],
-    openingHoursSpecification: [
-      {
-        "@type": "OpeningHoursSpecification",
-        dayOfWeek: "Friday",
-        opens: "19:30",
-        closes: "21:00",
-      },
-      {
-        "@type": "OpeningHoursSpecification",
-        dayOfWeek: "Saturday",
-        opens: "19:00",
-        closes: "21:00",
-      },
-      {
-        "@type": "OpeningHoursSpecification",
-        dayOfWeek: "Sunday",
-        opens: "14:30",
-        closes: "16:30",
-      },
-    ],
-    inLanguage: ["es", "en"],
   };
 
   return (
